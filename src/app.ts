58cc2f37f025b254f7261ec58b7532eb9e696198
hsur753 <hsur753@aucklanduni.ac.nz>
2021-04-03 01:27:31 +1300

    refactor: linting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-import bodyParser from "body-parser";
-import userRoute from "./Routes/User.Routes";
+import express, { Application, Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import userRoute from './Routes/User.Routes';
 
 const app: Application = express();
 
@@ -8,11 +8,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Root endpoint
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "Ok" });
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: 'Ok' });
 });
 
-app.use("/api/", userRoute);
+app.use('/api/', userRoute);
 
 // Insert here other API endpoints
 
