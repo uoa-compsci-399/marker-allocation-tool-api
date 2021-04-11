54c34d066a5a97b2e33d53028e93a38ddc0f831b
Isaac Kabel <79811008+ikab470-uoa@users.noreply.github.com>
2021-04-11 16:07:09 +1200

    Derive listen port from environment variable
    Rather than using the fixed port number 8000, check the environment variable PORT, and if it's number-like, use its floor. Otherwise, defer to the default of 8000.
    
    This is in line with the behavior offered by craco and webpack for the frontend, and is a feature the runtime will use to run two backends: main and develop branches.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from './app';
 
 // Server port
-const HTTP_PORT = 8000;
+const HTTP_PORT = parseInt(process.env.PORT || "") || 8000;
+
 // Start server
 app.listen(HTTP_PORT, () => {
   console.info(`Server is running on http://localhost:${HTTP_PORT}`);
