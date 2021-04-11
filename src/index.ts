212b7956cd2a5c8d4188f25c27852e3c7e40a0b5
Isaac Kabel <79811008+ikab470-uoa@users.noreply.github.com>
2021-04-11 16:08:35 +1200

    Lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
 // Server port
-const HTTP_PORT = parseInt(process.env.PORT || "") || 8000;
+const HTTP_PORT = parseInt(process.env.PORT || '') || 8000;
 
 // Start server
 app.listen(HTTP_PORT, () => {
