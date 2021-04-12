3d87c7784593e4151da56c2628a4052d21676c8b
Isaac Kabel <79811008+ikab470-uoa@users.noreply.github.com>
2021-04-12 18:37:10 +1200

    Amended database design to comply with Object Design document
    Named function in `DBDataUtil` as `initialize`, moved into a new class in that file with matching name.
    
    Moved call to close database outside serialize on the same database.
    
    Func'ed out createTable(), createTables(), populateTables(), initialize()
    
    Revised to CREATE TABLE statements to use only column defs, no table constraints.
    
    Replaced `function () {` syntax with arrow function syntax to be consistent.
    
    Changed import Database statement from SQLite3 module to use an `as` to avoid naming conflict with our own type.
    
    Moved `export default` to class definitions.
    
    Removed leading underscore of private member Database._db

--- src/db/DBDataUtil.ts
+++ src/db/DBDataUtil.ts
@@ -1,244 +1,186 @@
-import { Database } from 'sqlite3';
+import { Database as SQLite3Database } from 'sqlite3';
 
-const dbUtil = (db: Database): void => {
-  db.serialize(function () {
-    console.log('Creating tables...');
+export default class DBDataUtil {
+	private static createTable(db: SQLite3Database, name: string, cols: string[]): void {
+		const sqlCols = cols.join(", ");
+		const sqlCmd = `CREATE TABLE IF NOT EXISTS "${name}" (${sqlCols});`;
+		db.run(sqlCmd, (err: Error) => {
+			if(err == null) return;
+			console.error(err);
+		});
+	}
+	
+	static createTables(db: SQLite3Database): void {
+		db.serialize(() => {
+			this.createTable(db, "Course", [
+				'"courseID"	INTEGER PRIMARY KEY',
+				'"courseName"	TEXT NOT NULL',
+				'"year"	INTEGER NOT NULL',
+				'"whichSemestersField"	INTEGER NOT NULL',
+				'"isPublished"	INTEGER NOT NULL',
+				'"enrolmentEstimate"	INTEGER NOT NULL',
+				'"enrolmentFinal"	INTEGER NOT NULL',
+				'"workload"	INTEGER NOT NULL',
+				'"courseInfoDeadline"	TEXT',
+				'"applicationDeadline"	TEXT',
+				'"markerPrefDeadline"	TEXT',
+				'"markerAssignmentDeadline"	TEXT',
+				'"otherTasks"	TEXT',
+			]);
+			
+			this.createTable(db, "Application", [
+				'"applicationID"	INTEGER PRIMARY KEY',
+				'"markerID"	INTEGER NOT NULL REFERENCES "Marker"("userID")',
+				'"year"	INTEGER NOT NULL',
+				'"whichSemestersField"	INTEGER NOT NULL',
+				'"curriculumVitae"	BLOB NOT NULL',
+				'"academicRecord"	BLOB NOT NULL',
+				'"hoursRequested"	INTEGER NOT NULL',
+				'"relevantExperience"	TEXT',
+			]);
+			
+			this.createTable(db, "ApplicationCourse", [
+				'"applicationCourseID"	INTEGER PRIMARY KEY',
+				'"applicationID"	INTEGER NOT NULL REFERENCES "Application"("applicationID")',
+				'"courseID"	INTEGER NOT NULL REFERENCES "Course"("courseID")',
+				'"status"	INTEGER NOT NULL',
+				'"hoursAllocated"	INTEGER',
+				'"claimGradeAchieved"	TEXT'
+			]);
+			
+			this.createTable(db, "User", [
+				'"userID"	INTEGER PRIMARY KEY',
+				'"firstName"	TEXT NOT NULL',
+				'"lastName"	TEXT NOT NULL',
+				'"email"	TEXT NOT NULL',
+				'"role"	TEXT NOT NULL',
+			]);
+			
+			this.createTable(db, "MarkerCoordinator", [
+				'"userID"	INTEGER PRIMARY KEY REFERENCES "User"("userID")',
+				//'"markerCoordinatorAccess"	TEXT NOT NULL',
+			]);
+			
+			this.createTable(db, "CourseCoordinator", [
+				'"userID"	INTEGER PRIMARY KEY REFERENCES "User"("userID")',
+				//'"courseCoordinatorAccess"	TEXT NOT NULL',
+			]);
+			
+			this.createTable(db, "Marker", [
+				'"userID"	INTEGER PRIMARY KEY REFERENCES "User"("userID")',
+				//'"markerAccess"	TEXT NOT NULL',
+				'"upi" TEXT NOT NULL',
+				'"studentID" TEXT NOT NULL',
+				'"dateOfBirth"	TEXT NOT NULL',
+				'"phoneNumber"	TEXT',
+				'"isScholarshipRecipient"	INTEGER NOT NULL',
+				'"isNZCitizenOrPermanentResident"	INTEGER NOT NULL',
+				'"hasWorkVisa"	INTEGER NOT NULL',
+				'"isInAuckland"	INTEGER NOT NULL',
+				'"lastApplicationID"	INTEGER REFERENCES "Application"("applicationID")',
+			]);
+		});
+	}
+	
+	static populateTables(db: SQLite3Database): void {
+		db.serialize(() => {
+			// Create user data
+			const userInsert =
+				'INSERT OR IGNORE INTO User (userID, firstName, lastName, email, role) VALUES (?,?,?,?,?)';
+			const users = [
+				[1, 'Burkhard', 'Wuensche', 'burkhard@aucklanduni.ac.nz', 'MarkerCoordinator'],
+				[2, 'Asma', 'Shakil', 'asma@aucklanduni.ac.nz', 'CourseCoordinator'],
+				[3, 'Songyan', 'Teng', 'songyan@aucklanduni.ac.nz', 'Applicant'],
+				[4, 'Darren', 'Chen', 'darren@aucklanduni.ac.nz', 'Marker'],
+				[5, 'Jim', 'Park', 'jim@aucklanduni.ac.nz', 'Marker'],
+			];
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "User" (
-            "userID"	INTEGER NOT NULL,
-            "firstName"	TEXT NOT NULL,
-            "lastName"	TEXT NOT NULL,
-            "email"	TEXT NOT NULL,
-            "role"	TEXT NOT NULL,
-            PRIMARY KEY("userID"),
-            UNIQUE("email","userID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			users.forEach((user) => db.run(userInsert, user));
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "CourseCoordinator" (
-            "userID"	INTEGER NOT NULL,
-            "courseCoordinatorAccess"	TEXT NOT NULL,
-            PRIMARY KEY("userID"),
-            FOREIGN KEY("userID") REFERENCES "User"("userID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			// Create applicant data
+			const applicantInsert =
+				'INSERT OR IGNORE INTO Applicant (userID, degree, year, isEnrolled, GPA, hasVisa, inAuckland, academicRecord, applicantAccess) VALUES (?,?,?,?,?,?,?,?,?)';
+			const applicants = [[3, 'Science', 3, 'Yes', 5.0, 'Yes', 'Yes', '', 'Applicant Access']];
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "Marker" (
-            "userID"	INTEGER NOT NULL,
-            "markerAccess"	TEXT NOT NULL,
-            FOREIGN KEY("userID") REFERENCES "User"("userID"),
-            PRIMARY KEY("userID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			applicants.forEach((applicant) => db.run(applicantInsert, applicant));
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "MarkerCoordinator" (
-            "userID"	INTEGER NOT NULL,
-            "markerCoordinatorAccess"	TEXT NOT NULL,
-            PRIMARY KEY("userID"),
-            FOREIGN KEY("userID") REFERENCES "User"("userID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			// Create marker data
+			const markerInsert = 'INSERT OR IGNORE INTO Marker (userID, markerAccess) VALUES (?,?)';
+			const markers = [
+				[4, 'Marker Access'],
+				[5, 'Marker Access'],
+			];
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "Applicant" (
-            "userID"	INTEGER NOT NULL,
-            "degree"	TEXT NOT NULL,
-            "year"	INTEGER NOT NULL,
-            "isEnrolled"	TEXT NOT NULL,
-            "GPA"	REAL NOT NULL,
-            "hasVisa"	TEXT NOT NULL,
-            "inAuckland"	TEXT NOT NULL,
-            "academicRecord"	BLOB,
-            "applicantAccess"	TEXT NOT NULL,
-            FOREIGN KEY("userID") REFERENCES "User"("userID"),
-            PRIMARY KEY("userID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			markers.forEach((marker) => db.run(markerInsert, marker));
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "Course" (
-            "courseID"	INTEGER NOT NULL UNIQUE,
-            "courseName"	TEXT NOT NULL,
-            "enrolmentEstimate"	INTEGER NOT NULL,
-            "enrolmentFinal"	INTEGER NOT NULL,
-            "workload"	INTEGER NOT NULL,
-            "coordinatorID"	INTEGER NOT NULL,
-            "courseInfoDeadline"	TEXT,
-            "applicationDeadline"	TEXT,
-            "markerPrefDeadline"	TEXT,
-            "markerAssignmentDeadline"	TEXT,
-            "otherTasks"	TEXT,
-            FOREIGN KEY("coordinatorID") REFERENCES "CourseCoordinator"("userID"),
-            PRIMARY KEY("courseID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			// Create marker coordinator data
+			const markerCoordinatorInsert =
+				'INSERT OR IGNORE INTO MarkerCoordinator (userID, markerCoordinatorAccess) VALUES (?,?)';
+			const markerCoordinators = [[1, 'Marker Coordinator Access']];
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "Application" (
-            "applicationID"	INTEGER NOT NULL UNIQUE,
-            "applicantID"	INTEGER NOT NULL UNIQUE,
-            "courseID"	INTEGER NOT NULL UNIQUE,
-            "CV"	BLOB,
-            "relevantExperience"	TEXT,
-            FOREIGN KEY("applicantID") REFERENCES "Applicant"("userID"),
-            FOREIGN KEY("courseID") REFERENCES "Course"("courseID"),
-            PRIMARY KEY("applicationID")
-          );`
-    );
+			markerCoordinators.forEach((markerCoordinator) =>
+				db.run(markerCoordinatorInsert, markerCoordinator)
+			);
 
-    db.run(
-      `CREATE TABLE IF NOT EXISTS "MarkerAssignment" (
-            "markerID"	INTEGER NOT NULL,
-            "courseID"	INTEGER NOT NULL,
-            "year"	INTEGER NOT NULL,
-            "semester"	INTEGER NOT NULL,
-            "suitableForCourse"	TEXT,
-            FOREIGN KEY("courseID") REFERENCES "Course"("courseID"),
-            FOREIGN KEY("markerID") REFERENCES "Marker"("userID"),
-            PRIMARY KEY("markerID","courseID")
-          );`,
-      (err: Error) => {
-        if (err) {
-          // Table already created
-          return console.log(err.message);
-        }
-      }
-    );
+			// Create course coordinator data
+			const courseCoordinatorInsert =
+				'INSERT OR IGNORE INTO CourseCoordinator (userID, courseCoordinatorAccess) VALUES (?,?)';
+			const courseCoordinators = [[2, 'Course Coordinator Access']];
 
-    console.log('Inserting data...');
+			courseCoordinators.forEach((courseCoordinator) =>
+				db.run(courseCoordinatorInsert, courseCoordinator)
+			);
 
-    // Create user data
-    const userInsert =
-      'INSERT OR IGNORE INTO User (userID, firstName, lastName, email, role) VALUES (?,?,?,?,?)';
-    const users = [
-      [1, 'Burkhard', 'Wuensche', 'burkhard@aucklanduni.ac.nz', 'MarkerCoordinator'],
-      [2, 'Asma', 'Shakil', 'asma@aucklanduni.ac.nz', 'CourseCoordinator'],
-      [3, 'Songyan', 'Teng', 'songyan@aucklanduni.ac.nz', 'Applicant'],
-      [4, 'Darren', 'Chen', 'darren@aucklanduni.ac.nz', 'Marker'],
-      [5, 'Jim', 'Park', 'jim@aucklanduni.ac.nz', 'Marker'],
-    ];
+			// Create course data
+			const courseInsert =
+				'INSERT OR IGNORE INTO Course (courseID, courseName, enrolmentEstimate, enrolmentFinal, workload, coordinatorID, courseInfoDeadline, applicationDeadline, markerPrefDeadline, markerAssignmentDeadline, otherTasks) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
+			const courses = [
+				[
+					1,
+					'COMPSCI 399',
+					50,
+					100,
+					50,
+					2,
+					'01/01/2021',
+					'01/01/2021',
+					'01/01/2021',
+					'01/01/2021',
+					'Give good grades!',
+				],
+				[2, 'COMPSCI 101', 300, 500, 80, 2, '', '', '', '', ''],
+			];
 
-    users.forEach((user) => db.run(userInsert, user));
+			courses.forEach((course) => db.run(courseInsert, course));
 
-    // Create applicant data
-    const applicantInsert =
-      'INSERT OR IGNORE INTO Applicant (userID, degree, year, isEnrolled, GPA, hasVisa, inAuckland, academicRecord, applicantAccess) VALUES (?,?,?,?,?,?,?,?,?)';
-    const applicants = [[3, 'Science', 3, 'Yes', 5.0, 'Yes', 'Yes', '', 'Applicant Access']];
+			// Create application data
+			const applicationInsert =
+				'INSERT OR IGNORE INTO Application (applicationID, applicantID, courseID, CV, relevantExperience) VALUES (?,?,?,?,?)';
+			const applications = [[1, 3, 2, '', "I've taught stuff"]];
 
-    applicants.forEach((applicant) => db.run(applicantInsert, applicant));
+			applications.forEach((application) => db.run(applicationInsert, application));
 
-    // Create marker data
-    const markerInsert = 'INSERT OR IGNORE INTO Marker (userID, markerAccess) VALUES (?,?)';
-    const markers = [
-      [4, 'Marker Access'],
-      [5, 'Marker Access'],
-    ];
+			// Create marker assignment data
+			const markerAssignmentInsert =
+				'INSERT OR IGNORE INTO MarkerAssignment (markerID, courseID, year, semester, suitableForCourse) VALUES (?,?,?,?,?)';
+			const markerAssignments = [
+				[4, 1, 2021, 1, 'Yes'],
+				[5, 2, 2021, 2, 'No'],
+			];
 
-    markers.forEach((marker) => db.run(markerInsert, marker));
-
-    // Create marker coordinator data
-    const markerCoordinatorInsert =
-      'INSERT OR IGNORE INTO MarkerCoordinator (userID, markerCoordinatorAccess) VALUES (?,?)';
-    const markerCoordinators = [[1, 'Marker Coordinator Access']];
-
-    markerCoordinators.forEach((markerCoordinator) =>
-      db.run(markerCoordinatorInsert, markerCoordinator)
-    );
-
-    // Create course coordinator data
-    const courseCoordinatorInsert =
-      'INSERT OR IGNORE INTO CourseCoordinator (userID, courseCoordinatorAccess) VALUES (?,?)';
-    const courseCoordinators = [[2, 'Course Coordinator Access']];
-
-    courseCoordinators.forEach((courseCoordinator) =>
-      db.run(courseCoordinatorInsert, courseCoordinator)
-    );
-
-    // Create course data
-    const courseInsert =
-      'INSERT OR IGNORE INTO Course (courseID, courseName, enrolmentEstimate, enrolmentFinal, workload, coordinatorID, courseInfoDeadline, applicationDeadline, markerPrefDeadline, markerAssignmentDeadline, otherTasks) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
-    const courses = [
-      [
-        1,
-        'COMPSCI 399',
-        50,
-        100,
-        50,
-        2,
-        '01/01/2021',
-        '01/01/2021',
-        '01/01/2021',
-        '01/01/2021',
-        'Give good grades!',
-      ],
-      [2, 'COMPSCI 101', 300, 500, 80, 2, '', '', '', '', ''],
-    ];
-
-    courses.forEach((course) => db.run(courseInsert, course));
-
-    // Create application data
-    const applicationInsert =
-      'INSERT OR IGNORE INTO Application (applicationID, applicantID, courseID, CV, relevantExperience) VALUES (?,?,?,?,?)';
-    const applications = [[1, 3, 2, '', "I've taught stuff"]];
-
-    applications.forEach((application) => db.run(applicationInsert, application));
-
-    // Create marker assignment data
-    const markerAssignmentInsert =
-      'INSERT OR IGNORE INTO MarkerAssignment (markerID, courseID, year, semester, suitableForCourse) VALUES (?,?,?,?,?)';
-    const markerAssignments = [
-      [4, 1, 2021, 1, 'Yes'],
-      [5, 2, 2021, 2, 'No'],
-    ];
-
-    markerAssignments.forEach((markerAssignment) =>
-      db.run(markerAssignmentInsert, markerAssignment)
-    );
-
-    db.close();
-
-    console.log('Done!');
-  });
-};
-
-export default dbUtil;
+			markerAssignments.forEach((markerAssignment) =>
+				db.run(markerAssignmentInsert, markerAssignment)
+			);
+		});
+	}
+	
+	static initialize(db: SQLite3Database): void {
+		console.log('Creating tables...');
+		this.createTables(db);
+		
+		console.log('Inserting data...');
+		this.populateTables(db);
+		
+		console.log('Done!');
+	}
+}
--- src/db/Database.ts
+++ src/db/Database.ts
@@ -7,62 +7,60 @@ const DBSOURCE = 'db.sqlite';
 
 type Id = { id: number };
 
-class Database {
-  private _db: sqlite.Database;
+export default class Database {
+  private db: sqlite.Database;
 
   constructor() {
-    this._db = new sqlite3.Database(DBSOURCE, (err) => {
+    this.db = new sqlite3.Database(DBSOURCE, (err) => {
       if (err) {
         // Cannot open database
         console.error(err.message);
         throw err;
       } else {
-        DBDataUtil(this._db);
+        DBDataUtil.initialize(this.db);
       }
     });
   }
 
   run(sql: string, params: string[] = []): Promise<Id> {
     return new Promise<Id>((resolve, reject) => {
-      this._db = new sqlite3.Database(DBSOURCE);
-      this._db.run(sql, params, function (err) {
+      this.db = new sqlite3.Database(DBSOURCE);
+      this.db.run(sql, params, function (err) {
         if (err) {
           reject(err);
         } else {
           resolve({ id: this.lastID });
         }
       });
-      this._db.close();
+      this.db.close();
     });
   }
 
   get(sql: string, params: string[] = []): Promise<any> {
     return new Promise((resolve, reject) => {
-      this._db = new sqlite3.Database(DBSOURCE);
-      this._db.get(sql, params, (err, result) => {
+      this.db = new sqlite3.Database(DBSOURCE);
+      this.db.get(sql, params, (err, result) => {
         if (err) {
           reject(err);
         } else {
           resolve(result);
         }
       });
-      this._db.close();
+      this.db.close();
     });
   }
 
   all(sql: string, params: string[] = []): Promise<RequestBody> {
     return new Promise((resolve, reject) => {
-      this._db = new sqlite3.Database(DBSOURCE);
-      this._db.all(sql, params, (err, rows: RequestBody) => {
+      this.db = new sqlite3.Database(DBSOURCE);
+      this.db.all(sql, params, (err, rows: RequestBody) => {
         if (err) {
           reject(err);
         } else {
           resolve(rows);
         }
       });
-      this._db.close();
+      this.db.close();
     });
   }
 }
-
-export default Database;
