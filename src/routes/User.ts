233a36a6e7fb9d214ff55888efe301aa993c9308
Songyan Teng <songyanstteng@hotmail.com>
2021-04-16 23:32:37 +1200

    Added application-related endpoints

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,17 +1,17 @@
 import express, { Request, Response } from 'express';
 import db from '../db/DBController';
-import { RequestBody } from '../utils/RequestBody';
+import { UserRequest, ApplicationRequest } from '../utils/RequestBody';
 
 const router = express.Router();
 
 // Get a list of users
 router.get('/users', (req: Request, res: Response) => {
-  const sql = 'select * from User';
+  const sql = 'SELECT * FROM User';
   const params: string[] = [];
 
   db.all(sql, params).then(
     (value) => {
-      responseOk(res, value);
+      userResponseOk(res, value);
     },
     (reason: Error) => {
       badRequest(res, reason.message);
@@ -21,12 +21,12 @@ router.get('/users', (req: Request, res: Response) => {
 
 // Get a single user info(row) by userId
 router.get('/user/:userID', (req: Request, res: Response) => {
-  const sql = 'select * from user where userID = ?';
+  const sql = 'SELECT * FROM User WHERE userID = ?';
   const params = [req.params.userID];
 
   db.get(sql, params).then(
     (value) => {
-      responseOk(res, value);
+      userResponseOk(res, value);
     },
     (reason: Error) => {
       badRequest(res, reason.message);
@@ -38,7 +38,7 @@ router.get('/user/:userID', (req: Request, res: Response) => {
 router.post('/user/', (req: Request, res: Response) => {
   const errors = [];
 
-  const data = req.body as RequestBody;
+  const data = req.body as UserRequest;
 
   if (!data.userID) {
     errors.push('No userId specified');
@@ -62,9 +62,10 @@ router.post('/user/', (req: Request, res: Response) => {
 
   const sql = 'INSERT INTO User (userID, firstName, lastName, email, role) VALUES (?,?,?,?,?)';
   const params = [data.userID, data.firstName, data.lastName, data.email, data.role];
+
   db.run(sql, params).then(
     () => {
-      responseOk(res, data);
+      userResponseOk(res, data);
     },
     (reason: Error) => {
       badRequest(res, reason.message);
@@ -72,7 +73,88 @@ router.post('/user/', (req: Request, res: Response) => {
   );
 });
 
-const responseOk = (res: Response, data: RequestBody) => {
+// Get a list of applications
+router.get('/applications', (req: Request, res: Response) => {
+  const sql = 'SELECT * FROM Application';
+  const params: string[] = [];
+
+  db.all(sql, params).then(
+    (value) => {
+      applicationResponseOk(res, value);
+    },
+    (reason: Error) => {
+      badRequest(res, reason.message);
+    }
+  );
+});
+
+// Get a single user info(row) by applicationId
+router.get('/application/:applicationID', (req: Request, res: Response) => {
+  const sql = 'SELECT * FROM Application WHERE applicationID = ?';
+  const params = [req.params.applicationID];
+
+  db.get(sql, params).then(
+    (value) => {
+      applicationResponseOk(res, value);
+    },
+    (reason: Error) => {
+      badRequest(res, reason.message);
+    }
+  );
+});
+
+// POST Insert a user
+router.post('/application/', (req: Request, res: Response) => {
+  const errors = [];
+
+  const data = req.body as ApplicationRequest;
+
+  if (!data.applicationID) {
+    errors.push('No applicationId specified');
+  }
+  if (!data.applicantID) {
+    errors.push('No applicantID specified');
+  }
+  if (!data.courseID) {
+    errors.push('No courseID specified');
+  }
+  if (!data.CV) {
+    errors.push('No CV specified');
+  }
+
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(',') });
+    return;
+  }
+
+  const sql =
+    'INSERT INTO Application (applicationID, applicantID, courseID, CV, relevantExperience) VALUES (?,?,?,?,?)';
+  const params = [
+    data.applicationID,
+    data.applicantID,
+    data.courseID,
+    data.CV,
+    data.relevantExperience ? data.relevantExperience : '',
+  ];
+
+  db.run(sql, params).then(
+    () => {
+      applicationResponseOk(res, data);
+    },
+    (reason: Error) => {
+      badRequest(res, reason.message);
+    }
+  );
+});
+
+const userResponseOk = (res: Response, data: UserRequest) => {
+  res.json({
+    message: 'success',
+    data: data,
+  });
+};
+
+const applicationResponseOk = (res: Response, data: ApplicationRequest) => {
   res.json({
     message: 'success',
     data: data,
--- src/utils/RequestBody.ts
+++ src/utils/RequestBody.ts
@@ -1,7 +1,15 @@
-export interface RequestBody {
+export interface UserRequest {
   userID: string;
   firstName: string;
   lastName: string;
   email: string;
   role: string;
+}
+
+export interface ApplicationRequest {
+  applicationID: string;
+  applicantID: string;
+  courseID: string;
+  CV: string;
+  relevantExperience: string;
 }
