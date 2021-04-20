33e04e813635c15b219e72e6f8674018fcb68faa
hsur753 <hsur753@aucklanduni.ac.nz>
2021-04-21 00:13:42 +1200

    add cors + increase request file limit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import userRoute from './routes/User';
+import cors from 'cors';
 
 const app: Application = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors());
 
 // Root endpoint
 app.get('/', (req: Request, res: Response) => {
