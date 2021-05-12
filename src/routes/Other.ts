97de5bab27e670d948db25dd507808724f3512a9
Songyan Teng <songyanstteng@hotmail.com>
2021-05-12 14:42:05 +1200

    Added routes for allocated markers list, count of applications for each course

--- src/routes/Other.ts
+++ src/routes/Other.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from 'express';
 
 import db from '../db/DBController';
-import {
-  RequestBody,
-} from '../utils/RequestBody';
+import { RequestBody } from '../utils/RequestBody';
 
 const router = express.Router();
 
@@ -15,6 +13,44 @@ router.get('/course/:courseID/applications', (req: Request, res: Response) => {
                AND a.applicationID = ac.applicationID
                AND a.markerID = m.userID
                AND m.userID = u.userID`;
+  const params = [req.params.courseID];
+
+  db.all(sql, params).then(
+    (value) => {
+      responseOk(res, value);
+    },
+    (reason: Error) => {
+      badRequest(res, reason.message);
+    }
+  );
+});
+
+// Get a list of allocated markers for a course
+router.get('/course/:courseID/markers', (req: Request, res: Response) => {
+  const sql = `SELECT u.userID, u.firstName || ' ' || u.lastName AS [markerName], ac.hoursAllocated
+               FROM Application a, ApplicationCourse ac, Marker m, User u
+               WHERE ac.courseID = ?
+               AND a.applicationID = ac.applicationID
+               AND a.markerID = m.userID
+               AND m.userID = u.userID
+               AND ac.status = 1`;
+  const params = [req.params.courseID];
+
+  db.all(sql, params).then(
+    (value) => {
+      responseOk(res, value);
+    },
+    (reason: Error) => {
+      badRequest(res, reason.message);
+    }
+  );
+});
+
+// Get a count of the current number of applications for a course
+router.get('/course/:courseID/applicationcount', (req: Request, res: Response) => {
+  const sql = `SELECT COUNT(applicationCourseID) AS [count]
+               FROM ApplicationCourse ac
+               WHERE ac.courseID = ?`;
   const params = [req.params.courseID];
 
   db.all(sql, params).then(
