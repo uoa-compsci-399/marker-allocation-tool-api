812f6556b5fa0fb90488f35d9c0a2d47d4e19ed3
Isaac Kabel <79811008+ikab470-uoa@users.noreply.github.com>
2021-05-19 18:31:53 +1200

    Created utility function AuthnUtil.assertMinimumPrivilegeLevel()

--- src/utils/AuthnUtil.ts
+++ src/utils/AuthnUtil.ts
@@ -3,6 +3,23 @@ import { Request } from 'express';
 import db from '../db/DBController';
 import { UserRow } from '../db/DatabaseTypes';
 
+export enum PrivilegeLevel {
+  None,
+  LoggedIn,
+  /* Marker is currently the lowest privilege level, but isn't defined as a synonym for LoggedIn
+   * for extensibility
+   */
+  Marker,
+  CourseCoordinator,
+  MarkerCoordinator,
+}
+
+const map_role_to_privilege_level: Record<string, PrivilegeLevel> = {
+  Marker: PrivilegeLevel.Marker,
+  CourseCoordinator: PrivilegeLevel.CourseCoordinator,
+  MarkerCoordinator: PrivilegeLevel.MarkerCoordinator,
+};
+
 export default class AuthnUtil {
   static async getCurrentUser(req: Request): Promise<UserRow | boolean> {
     const tokenValue = (req.cookies as Record<string, string>)['authn_token'];
@@ -13,5 +30,19 @@ export default class AuthnUtil {
     if (user == null) return false;
 
     return user;
+  }
+
+  static async assertMinimumPrivilegeLevel(
+    req: Request,
+    minimum_level: PrivilegeLevel
+  ): Promise<boolean> {
+    if (minimum_level < PrivilegeLevel.LoggedIn) return true;
+
+    const user = await this.getCurrentUser(req);
+
+    if (minimum_level >= PrivilegeLevel.LoggedIn && user == false) return false;
+
+    const user_level = map_role_to_privilege_level[(user as UserRow).role];
+    return user_level >= minimum_level;
   }
 }
